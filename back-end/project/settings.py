2dab6c58b51b44f1a4b81ac8846533c76c47b050
Omer Yanai <omery33111@gmail.com>
2024-07-24 15:41:58 +0300

    Deployment

--- back-end/project/settings.py
+++ back-end/project/settings.py
@@ -211,20 +211,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# STATIC_URL = '/staticBack/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    BASE_DIR / 'Front-end/build/static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'staticBack',
+#     BASE_DIR / 'Front-end/build/static'
+# ]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticBack'),
+# ]
 
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'staticBack/images')
 
-
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
@@ -240,8 +241,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
